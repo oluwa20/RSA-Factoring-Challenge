7884269174d104200456fee34ce5d8ab8d5ca6d4
Faisal Ali-Adebambo <oluwabori2004@gmail.com>
2024-01-28 20:53:46 -0800

    made great changes

--- test/random.sh
+++ test/random.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 # Specify the number of files you want to create
-num_files=60
+num_files=54
+
+# Function to generate a random prime number
+generate_prime() {
+    echo $(shuf -i 10-100 -n 1)
+}
 
 # Loop through the desired number of files
 for ((i=1; i<=num_files; i++)); do
-    # Generate a random number (adjust the range as needed)
-    random_number=$((RANDOM % 100 + 1))
+    # Generate two random prime numbers
+    prime1=$(generate_prime)
+    prime2=$(generate_prime)
 
-    # Write the random number to the file
-    echo $random_number > "rsa-$i"
+    # Calculate the factorizable number
+    factorizable_number=$((prime1 * prime2))
+
+    # Write the factorizable number to the file
+    echo $factorizable_number > "rsa-$i"
 done
 
